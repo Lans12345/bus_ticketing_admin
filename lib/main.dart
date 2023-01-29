aa536a74e5501313896a205ec08eceded25fc069
Lans12345 <olanalans12345@gmail.com>
2023-01-30 07:56:33 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:bus_ticketing_admin/screens/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyA9oRErfzlwcF7qOd3cRU_Ed8av5uIRog0",
+    appId: "1:324995757513:web:041c7eb3c7301d8663a418",
+    messagingSenderId: "324995757513",
+    projectId: "bus-ticketing-c01a8",
+  ));
   runApp(const MyApp());
 }
 
