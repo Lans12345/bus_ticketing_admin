1bc9ff541b94e4721fa403140f2916d3830a5294
Lans12345 <olanalans12345@gmail.com>
2023-01-30 07:34:50 +0800

    added tab

--- lib/screens/tabs/rides_tab.dart
+++ lib/screens/tabs/rides_tab.dart
@@ -6,67 +6,138 @@ class RidesTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: 30,
-        ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            Align(
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.grey[200],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 30,
+          ),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: BoldText(
+                      label: 'Ride History',
+                      fontSize: 38,
+                      color: Colors.black)),
+              const SizedBox(
+                height: 30,
+              ),
+              Align(
                 alignment: Alignment.topLeft,
-                child: BoldText(
-                    label: 'List of Rides', fontSize: 38, color: Colors.black)),
-            const SizedBox(
-              height: 30,
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Card(
-                child: Container(
-                  color: Colors.white,
-                  height: 150,
-                  width: 300,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.bus_alert_sharp,
-                              size: 54,
-                              color: Colors.black,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            BoldText(
-                                label: 'No. of Rides',
-                                fontSize: 24,
-                                color: Colors.black),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Align(
-                          alignment: Alignment.bottomCenter,
-                          child: BoldText(
-                              label: '20', fontSize: 38, color: Colors.black),
-                        ),
-                      ],
+                child: Card(
+                  child: Container(
+                    color: Colors.white,
+                    height: 150,
+                    width: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.bus_alert_sharp,
+                                size: 54,
+                                color: Colors.black,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              BoldText(
+                                  label: 'No. of Rides',
+                                  fontSize: 24,
+                                  color: Colors.black),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: BoldText(
+                                label: '20', fontSize: 38, color: Colors.black),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: DataTable(columns: [
+                  DataColumn(
+                      label: NormalText(
+                          label: 'No.', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Name', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Email', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Address', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Origin', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Destination',
+                          fontSize: 18,
+                          color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Fare', fontSize: 18, color: Colors.black)),
+                ], rows: [
+                  for (int i = 0; i < 50; i++)
+                    DataRow(cells: [
+                      DataCell(
+                        NormalText(
+                            label: i.toString(),
+                            fontSize: 16,
+                            color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'name', fontSize: 16, color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'email', fontSize: 16, color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'address', fontSize: 16, color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'origin ', fontSize: 16, color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'destination',
+                            fontSize: 16,
+                            color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'fare', fontSize: 16, color: Colors.grey),
+                      ),
+                    ])
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/tabs/users_tab.dart
+++ lib/screens/tabs/users_tab.dart
@@ -6,69 +6,120 @@ class UsersTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: 30,
-        ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            Align(
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.grey[200],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 30,
+          ),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: BoldText(
+                      label: 'User Management',
+                      fontSize: 38,
+                      color: Colors.black)),
+              const SizedBox(
+                height: 30,
+              ),
+              Align(
                 alignment: Alignment.topLeft,
-                child: BoldText(
-                    label: 'User Management',
-                    fontSize: 38,
-                    color: Colors.black)),
-            const SizedBox(
-              height: 30,
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Card(
-                child: Container(
-                  color: Colors.white,
-                  height: 150,
-                  width: 300,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.person,
-                              size: 54,
-                              color: Colors.black,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            BoldText(
-                                label: 'No. of Users',
-                                fontSize: 24,
-                                color: Colors.black),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Align(
-                          alignment: Alignment.bottomCenter,
-                          child: BoldText(
-                              label: '20', fontSize: 38, color: Colors.black),
-                        ),
-                      ],
+                child: Card(
+                  child: Container(
+                    color: Colors.white,
+                    height: 150,
+                    width: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.person,
+                                size: 54,
+                                color: Colors.black,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              BoldText(
+                                  label: 'No. of Users',
+                                  fontSize: 24,
+                                  color: Colors.black),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: BoldText(
+                                label: '20', fontSize: 38, color: Colors.black),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: DataTable(columns: [
+                  DataColumn(
+                      label: NormalText(
+                          label: 'No.', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Name', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Email', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'Address', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: NormalText(
+                          label: 'ID', fontSize: 18, color: Colors.black)),
+                ], rows: [
+                  for (int i = 0; i < 50; i++)
+                    DataRow(cells: [
+                      DataCell(
+                        NormalText(
+                            label: i.toString(),
+                            fontSize: 16,
+                            color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'name', fontSize: 16, color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'email', fontSize: 16, color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'address', fontSize: 16, color: Colors.grey),
+                      ),
+                      DataCell(
+                        NormalText(
+                            label: 'id', fontSize: 16, color: Colors.grey),
+                      ),
+                    ])
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
